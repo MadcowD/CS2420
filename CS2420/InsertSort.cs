c1d2813ad2cdec4bcef0d6feb02226f37ae9bd2f
MadcowDeity <carlostnb@gmail.com>
2014-05-17 12:02:17 -0600

    Setting up the proj.

--- CS2420/InsertSort.cs
+++ CS2420/InsertSort.cs
@@ -18,19 +18,25 @@ namespace Sorting
         /// <returns>The sorted list.</returns>
         public IList<IComparable> Sort(IList<IComparable> unsorted)
         {
+            //Convert unsorted into a non fixed size list
+
+            
             //1. Manipulate original array
-            for(int i = 0; i < unsorted.Count; i++)
+            for(int i = 1; i < unsorted.Count; i++)
             {
                 IComparable point = unsorted[i];
                 unsorted.RemoveAt(i);
 
                 //Check the elements behind the point.
-                for (int j = i < unsorted.Count ? i : i-1; j >= 0; j--)
-                    if (unsorted[j].CompareTo(point) == -1 || j == 0) 
-                    { 
-                        unsorted.Insert(j, point);
+                for (int j = i-1; j >= -1; j--)
+                    if (j == -1 || point.CompareTo(unsorted[j]) >= 0)
+                    {
+               
+                        unsorted.Insert(j+1, point);
                         break;
                     }
+
+                Console.WriteLine(unsorted.Render());
             }
 
             return unsorted;
--- CS2420/Program.cs
+++ CS2420/Program.cs
@@ -11,8 +11,9 @@ namespace Sorting
         static void Main(string[] args)
         {
             InsertSort insAlg = new InsertSort();
-            Console.WriteLine(insAlg.Sort).Render());
-            insAlg.Sort(new int[] { 1, 2 }.ToList());
+            var list = new IComparable[] {1,10,6,13,4,3,2,1,1,6,5,3,7,4,1}.ToList();
+            Console.WriteLine(insAlg.Sort(list).Render());
+            Console.ReadKey();
         }
 
     }
