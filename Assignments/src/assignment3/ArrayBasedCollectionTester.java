4f9f55047f23001609b954e211e244af0b183fc7
Maks Cegielski-Johnson <maksus01@hotmail.com>
2014-06-04 22:33:14 -0600

    Update ArrayBasedCollectionTester.java

--- Assignments/src/assignment3/ArrayBasedCollectionTester.java
+++ Assignments/src/assignment3/ArrayBasedCollectionTester.java
@@ -1,19 +1,16 @@
-/**
- * 
- */
 package assignment3;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
-
 import junit.framework.TestCase;
+
 
 /**
  * @author Maks Cegielski-Johnson
  * @author William Guss
  * 
- * ArrayBasedCollection testing using JUnit
+ * ArrayBasedCollection testing on Integers using JUnit
  * 
  * TODO ADD ANYMORE COMMENTS?? 
  *
@@ -21,7 +18,7 @@ import junit.framework.TestCase;
 public class ArrayBasedCollectionTester extends TestCase {
 
 	ArrayBasedCollection<Integer> arr;
-	ArrayBasedCollection<String> str;
+	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
@@ -30,23 +27,18 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#ArrayBasedCollection()}.
+	 * Tests whether the constructor is working correctly
 	 */
 	public void testArrayBasedCollection() {
 		arr = new ArrayBasedCollection<Integer>();
-		assertEquals(arr.size,0);
+		assertEquals(arr.size,0); //check if size is zero
 		assertNotNull(arr.data);
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#add(java.lang.Object)}.
-	 */
-	public void testAddString() {
-		str = new ArrayBasedCollection<String>();
-
-	}
-
-	
+	 * Tests whether items added to the list remain unique to make sure multiples are not added
+	 * 
+	 */ 
 	public void testAddUnique(){
 		arr = new ArrayBasedCollection<Integer>();
 		assertEquals(arr.add(3), true);
@@ -54,6 +46,10 @@ public class ArrayBasedCollectionTester extends TestCase {
 
 	}
 
+	/**
+	 * Tests whether items added to the list remain in the order in which they were added	 * 
+	 * 
+	 */
 	public void testAddOrder(){
 		arr = new ArrayBasedCollection<Integer>();
 		arr.add(3);
@@ -70,18 +66,21 @@ public class ArrayBasedCollectionTester extends TestCase {
 
 	}
 
+	/**
+	 * Assures that the data grows by a factor of 2 when reaching capacity
+	 */
 	public void testAddGrowth(){
 		arr = new ArrayBasedCollection<Integer>();
 		assertEquals(arr.getBounds(), 10);
-		for(int i = 0; i<12; i++){
+		for(int i = 0; i<12; i++){//Adds more than 10 items
 			arr.add(i);
 		}
-		assertEquals(arr.getBounds(), 20);
+		assertEquals(arr.getBounds(), 20);//data should be 20
 
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#addAll(java.util.Collection)}.
+	 * Assures that a list is added to the collection and if no elements are added then returns false
 	 */
 	public void testAddAll() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -96,7 +95,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#clear()}.
+	 * Assures that a collection containing any items is empty after peforming the clear method
 	 */
 	public void testClear() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -109,7 +108,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#contains(java.lang.Object)}.
+	 * Assures that the set of integers {10...19} contains 15 and doesn't contain -5
 	 */
 	public void testContains() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -120,18 +119,22 @@ public class ArrayBasedCollectionTester extends TestCase {
 		assertEquals(arr.contains(-5), false);
 	}
 
+	/**
+	 * Assures that contains works on an empty collection
+	 * 
+	 */
 	public void testEmptyContains(){
 		arr = new ArrayBasedCollection<Integer>();
 		assertEquals(arr.contains(0), false);
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#containsAll(java.util.Collection)}.
+	 * Assures that every item in "even" is contained in the collection and that no items from "big" are in the collection
 	 */
 	public void testContainsAll() {
-		ArrayList<Integer> even = new ArrayList<Integer>();
-		ArrayList<Integer> big = new ArrayList<Integer>();
-		arr = new ArrayBasedCollection<Integer>();
+		ArrayList<Integer> even = new ArrayList<Integer>();//The set with elements e such that {e = 2k | 1 <= k <=5}
+		ArrayList<Integer> big = new ArrayList<Integer>(); //The set with elements m such that {11 <= m <= 20}
+		arr = new ArrayBasedCollection<Integer>(); // The set with elements n such that { 1 <= n <= 10}
 		for(int i = 1; i<=20; i++){
 			if(i<=10){
 				arr.add(i);
@@ -151,7 +154,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#isEmpty()}.
+	 * Asserts that isEmpty is true when the collection is empty and false otherwise
 	 */
 	public void testIsEmpty() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -163,7 +166,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#iterator()}.
+	 * Asserts that the Iterator functions for the collection are working correctly
 	 */
 	public void testIterator() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -187,7 +190,8 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#remove(java.lang.Object)}.
+	 * Asserts that items that are in the collection are removed when called to do so, returning true, and false if 
+	 * the element does not exist in the collection
 	 */
 	public void testRemove() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -201,7 +205,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#removeAll(java.util.Collection)}.
+	 * Series of tests that asserts that removing all items of a list from the collection works properly
 	 */
 	public void testRemoveAll() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -239,7 +243,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#size()}.
+	 * Assures that size works correctly after calling some remove and add functions
 	 */
 	public void testSize() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -253,7 +257,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#toArray()}.
+	 * Asserts that the toArray method correctly creates the proper array
 	 */
 	public void testToArray() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -268,7 +272,7 @@ public class ArrayBasedCollectionTester extends TestCase {
 
 
 	/**
-	 * Test method for {@link assignment3.ArrayBasedCollection#toSortedList(java.util.Comparator)}.
+	 * Assures that the sort method correctly sorts a shuffled list, using a trivial isSorted helper method 
 	 */
 	public void testToSortedList() {
 		arr = new ArrayBasedCollection<Integer>();
@@ -286,17 +290,16 @@ public class ArrayBasedCollectionTester extends TestCase {
 		
 		for(int i : arr)
 			unsort.add(i);
-		assertEquals(isSorted(unsort), false);
+		assertEquals(isSorted(unsort), false);//Assures that the list is unsorted and that the isSorted method works
 		
 		
 		ArrayList<Integer> sorted = arr.toSortedList(cmp);
 		
-		assertEquals(isSorted(sorted), true);
-		//TODO WHATDO????? HALP
+		assertEquals(isSorted(sorted), true);//Assuming the previous assertEquals holds, this proves that the collection is sorted correctly
 	}
 	
 	/**
-	 * TESTING FOR BINARY SEARCH
+	 * Assures that the binary search correctly finds values that are contained in the list and that are not
 	 */
 	public void testBinarySearch(){
 		arr = new ArrayBasedCollection<Integer>();
@@ -308,17 +311,19 @@ public class ArrayBasedCollectionTester extends TestCase {
 				list.add(i);
 		}
 		
-		assertEquals(SearchUtil.binarySearch(list, 65, cmp), true); //TODO HELP ME WITH THIS
+		assertEquals(SearchUtil.binarySearch(list, 65, cmp), true); 
 		assertEquals(SearchUtil.binarySearch(list, 73, cmp), false);
+		assertEquals(SearchUtil.binarySearch(list, 0, cmp), true); 
+		assertEquals(SearchUtil.binarySearch(list, 95, cmp), true); 
 		
 		
 		
 	}
 	
 	/**
-	 * Checks whether a given ArrayList is sorted or not
+	 * Helper method that checks whether a given ArrayList is sorted or not
 	 * @param list
-	 * @return
+	 * @return boolean true of false if the list is sorted or not
 	 */
 	private static boolean isSorted(ArrayList<Integer> list){
 		for(int i = 0; i<list.size()-1; i++){
