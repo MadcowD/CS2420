de9486078e2b3e6a5e6752c992e7464db3019424
MadcowD <carlostnb@gmail.com>
2014-06-24 11:32:21 -0600

    Binary search tree implementation.

--- Assignments/src/assignment6/BinarySearchTree.java
+++ Assignments/src/assignment6/BinarySearchTree.java
@@ -8,215 +8,224 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-
-//MUST CONTAIN NO DUPLICATES SINCE IT IS A SET
-
-/**
- * 
- * @author Maks Cegielski-Johnson
- * @author William Guss
- * @author Paymon Saebi
- *
- * @param <Type>
- */
 public class BinarySearchTree<Type extends Comparable<? super Type>> implements SortedSet<Type>, TreeTraversal<Type>{
-	public static void main(String[] args){
-		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-		bst.add(5);
-		bst.add(6);
-		bst.add(1);
-		bst.add(13);
-		bst.add(-6);
-	}
-
-
-	private BinaryNode root;
+	private BinaryNode root = null;
 
 	public BinarySearchTree(){
-		root = null;
-
-	}
-
-	@Override
-	public List<Type> inOrderDFT() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Type> preOrderDFT() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Type> postOrderDFT() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Type> levelOrderBFT() {
-		LinkedList<BinaryNode> list = new LinkedList<BinaryNode>();
-		//TODO
-		return null;
 	}
 
 	@Override
 	public boolean add(Type item) {
-		if(root == null){
+		// TODO Auto-generated method stub
+		if(item == null)
+			throw new NullPointerException();
+
+		if(isEmpty())
+		{
 			root = new BinaryNode(item);
 			return true;
 		}
-		return add(item, root);
-
+		else
+			return root.add(item);
 	}
 
-	private boolean add(Type item, BinaryNode stand){
-		if(item.compareTo(stand.data) == 0)
-			return false;
-		else if(item.compareTo(stand.data) < 0){
-			if(stand.left == null){
-				stand.left = new BinaryNode(item);
-				return true;
-			}
-			else
-				return add(item, stand.left);
-		}
-		else{
-			if(stand.right == null){
-				stand.right = new BinaryNode(item);
-				return true;
-			}
-			else
-				return add(item, stand.right);
-		}
-	}
+
 
 	@Override
 	public boolean addAll(Collection<? extends Type> items) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean changed = false;
+		for(Type item : items)
+			if(this.add(item))
+				changed = true;
+
+		return changed;
 	}
+
+
 
 	@Override
 	public void clear() {
-		root.left = null;
-		root.right = null;
+		// We simply let java's garbage collector do the work by setting the root to null
+		root = null;
+
 	}
+
+
 
 	@Override
 	public boolean contains(Type item) {
+		//check for null
+		if(item == null)
+			throw new NullPointerException();
 
-		return contains(item, root);
-	}
-
-	private boolean contains(Type item, BinaryNode t){
-		if(t == null)
+		if(isEmpty())
 			return false;
-		if(item.equals(t.data))
-			return true;
-		if(item.compareTo(t.data) < 0)
-			return contains(item, t.left);
 		else
-			return contains(item, t.right);
+			return root.contains(item);
 
 	}
+
 
 	@Override
 	public boolean containsAll(Collection<? extends Type> items) {
-		// TODO Auto-generated method stub
-		return false;
+		for(Type item : items){
+			if(!contains(item))
+				return false;
+		}
+		return true;
 	}
+
+
 
 	@Override
 	public boolean isEmpty() {
-		return (root.right == null) && (root.left == null);
+		return root == null;
 	}
+
+
 
 	@Override
 	public boolean remove(Type item) {
-		
-		return false;
+		if(item == null)
+			throw new NullPointerException();
+
+		if(isEmpty())
+			return false;
+		else{
+			if( root.remove(item)){
+				//if there is only one item and that item is the root.
+				if(root.getData() == null)
+					root = null;
+				return true;
+			}
+			else
+				return false;
+		}
 	}
+
+
 
 	@Override
 	public boolean removeAll(Collection<? extends Type> items) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean changed = false;
+		for(Type item : items)
+			if(remove(item))
+				changed = true;
+
+		return changed;
 	}
+
+
 
 	@Override
 	public int size() {
-		return root.numChildren();
+		return root == null ? 0 : root.size();
 	}
 
+
+
+	/**
+	 * This is just unfortunate. -_-
+	 */
 	@Override
 	public ArrayList<Type> toArrayList() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Type>(this.inOrderDFT());
 	}
+
+
 
 	@Override
 	public Type first() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return root.getLeftmostNode().data;
+		if(isEmpty())
+			throw new NoSuchElementException();
+
+		return root.getLeftmostNode().getData();
 	}
+
+
 
 	@Override
 	public Type last() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return root.getRightmostNode().data;
+		if(isEmpty())
+			throw new NoSuchElementException();
+
+		return root.getRightmostNode().getData();
+	}
+
+
+	//----------------------------------
+	// TRAVERSAL
+	//----------------------------------
+
+	@Override
+	public List<Type> inOrderDFT() {
+		ArrayList<Type> result = new ArrayList<Type>();
+		if(!isEmpty())
+			root.inOrderDFT(result);
+
+		return result;
 	}
 
 	@Override
-	public void writeDot(String filename)
-	{
-		try 
-		{
-			// PrintWriter(FileWriter) will write output to a file
-			PrintWriter output = new PrintWriter(new FileWriter(filename));
+	public List<Type> preOrderDFT() {
+		ArrayList<Type> result = new ArrayList<Type>();
+		if(!isEmpty())
+			root.preOrderDFT(result);
 
-			// Set up the dot graph and properties
-			output.println("digraph BST {");
-			output.println("node [shape=record]");
+		return result;
+	}
 
-			if(root != null)
-				writeDotRecursive(root, output);
-			// Close the graph
-			output.println("}");
-			output.close();
+	@Override
+	public List<Type> postOrderDFT() {
+		ArrayList<Type> result = new ArrayList<Type>();
+		if(!isEmpty())
+			root.postOrderDFT(result);
+
+		return result;
+	}
+
+	@Override
+	public List<Type> levelOrderBFT() {
+		ArrayList<Type> result = new ArrayList<Type>();
+		LinkedList<BinaryNode> queue = new LinkedList<BinaryNode>();
+
+		queue.addFirst(root);
+		while(!queue.isEmpty()){
+			BinaryNode n = queue.removeLast();
+			result.add(n.getData());
+			if(n.getLeft() != null)
+				queue.add(n.getLeft());
+			if(n.getRight() != null)
+				queue.add(n.getRight());
+		}
+
+		return result;
+	}
+
+
+	@Override
+	public void writeDot(String filename) {
+		if(isEmpty())
+			return;
+		try{
+			FileWriter fw = new FileWriter(filename);
+
+			root.writeDot(fw);
+
+
+			fw.close();
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
+
 	}
 
 
-	// Recursive method for writing the tree to  a dot file
-	private void writeDotRecursive(BinaryNode n, PrintWriter output) throws Exception
-	{
-		output.println(n.data + "[label=\"<L> |<D> " + n.data + "|<R> \"]");
 
-		if(n.left != null)
-		{
-			// write the left subtree
-			writeDotRecursive(n.left, output);
-
-			// then make a link between n and the left subtree
-			output.println(n.data + ":L -> " + n.left.data + ":D" );
-		}
-		if(n.right != null)
-		{
-			// write the left subtree
-			writeDotRecursive(n.right, output);
-
-			// then make a link between n and the right subtree
-			output.println(n.data + ":R -> " + n.right.data + ":D" );
-		}		
-	}
-
+	// YOU CAN USE THE PRIVATE CLASS BELOW AS YOUR NODE FOR ASSIGNMENT 6
 	/**
 	 * Represents a general binary tree node. Each binary node contains
 	 * data, a left child, and a right child, and a parent.
@@ -233,7 +242,6 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 
 		BinaryNode right;
 
-
 		/**
 		 * Construct a new node with known children
 		 * 
@@ -243,6 +251,110 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			data = _data;
 			left = _left;
 			right = _right;
+		}
+
+		public void inOrderDFT(List<Type> result) {
+			if(getLeft() != null)
+				getLeft().inOrderDFT(result);
+			result.add(this.data);
+			if(getRight() != null)
+				getRight().inOrderDFT(result);
+		}
+
+		public void preOrderDFT(List<Type> result){
+			result.add(this.data);
+
+			if(getLeft() != null)
+				getLeft().preOrderDFT(result);
+
+			if(getRight() != null)
+				getRight().preOrderDFT(result);
+
+		}
+
+		public void postOrderDFT(List<Type> result){
+			if(getLeft() != null)
+				getLeft().postOrderDFT(result);
+
+			if(getRight() != null)
+				getRight().postOrderDFT(result);
+
+			result.add(this.data);
+		}
+
+
+		public boolean remove(Type item) {
+			int compare = item.compareTo(this.data);
+
+			if(compare == 0){
+				//Actually remove the node by setting data to null
+				// The dereferencing occurs on the recursive closure of the stack
+				if(!hasChildren()){
+					this.data = null;
+					return true;
+				}
+			}
+			else if(compare > 0){
+				if(getRight() != null){
+					if(getRight().getData().compareTo(item) == 0)
+						this.right = right.getSuccessor();
+					else
+						getRight().remove(item);
+				}
+				else
+					return false;
+			}
+			else if(compare < 0)
+			{
+				if(getLeft() != null){
+					if(getLeft().getData().compareTo(item) == 0)
+						this.left = left.getSuccessor();
+					else
+						getLeft().remove(item);
+				}
+
+				else
+
+					return false;
+			}
+
+			//unreachable code.
+			return false;
+		}
+
+
+		/**
+		 * Returns whether or not any children exist for the given binary node.
+		 * @return
+		 */
+		public boolean hasChildren(){
+			return this.right != null || this.left != null;
+		}
+
+		public boolean add(Type item) {
+			int compare = item.compareTo(data);
+			if(compare == 0)
+				return false;
+			else if(compare > 0)
+			{
+				if(getRight() != null)
+					return getRight().add(item);
+				else{
+					this.setRight(new BinaryNode(data));
+					return true;
+				}
+			}
+			else if(compare < 0){
+				if(getLeft() != null)
+					return getLeft().add(item);
+				else{
+					this.setLeft(new BinaryNode(data));
+					return true;
+				}
+			}
+
+			//Unreachable code
+			return false;
 		}
 
 		/**
@@ -318,7 +430,6 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			right = _right;
 		}
 
-
 		/**
 		 * Number of children
 		 * Use this to help figure out which BST deletion case to perform
@@ -337,6 +448,21 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			return numChildren;
 		}
 
+
+		public int size(){
+
+			int size = 1;
+
+			if(getLeft() != null)
+				size += getLeft().size();
+			if(getRight() != null)
+				size += getRight().size();
+
+
+			return size;
+
+		}
+
 		/**
 		 * @return The leftmost node in the binary tree rooted at this node.
 		 */
@@ -346,9 +472,8 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			if(getLeft() == null)
 				return this; // returns "this" node
 
-
 			// FILL IN - do not return null
-			return this.left.getLeftmostNode();
+			return getLeft().getLeftmostNode();
 		}
 
 		/**
@@ -361,7 +486,7 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 				return this; // returns "this" node
 
 			// FILL IN - do not return null
-			return this.right.getRightmostNode();
+			return getRight().getRightmostNode();
 		}		
 
 		/** 
@@ -375,9 +500,16 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		 */
 		public BinaryNode getSuccessor() 
 		{			
-			if(this.right == null)
-				return this.left.getRightmostNode();
-			return this.right.getLeftmostNode();
+			if(hasChildren()){
+				if(getLeft() == null && getRight() != null)
+					return getRight();
+				else if(getLeft() != null && getRight() == null)
+					return getLeft();
+				else // the two children case
+					return getRight().getLeftmostNode();
+			}
+
+			return null;
 		}
 
 		/**
@@ -390,15 +522,61 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		 */
 		public int height() 
 		{			
-			if(this == null)
-				return -1;
-			int lefth = this.left.height();
-			int righth = this.right.height();
+			// FILL IN - do not return -1
 
-			if(lefth > righth)
-				return lefth + 1;
-			else
-				return righth + 1;
+			int rh = getRight() == null ? -1 : getRight().height();
+			int lh = getLeft() == null ? -1 : getLeft().height();
+			// FILL IN - do not return -1
+			return 1 + (rh > lh ? rh : lh);
 		}
-	}	
+
+		public boolean contains(Type elem){
+			int compare = elem.compareTo(this.data);
+			if(compare == 0)
+				return true;
+			else if(compare > 0){
+				if(getRight() != null)
+					return getRight().contains(elem);
+				else
+					return false;
+			}
+			else if(compare < 0){
+				if(getLeft() != null)
+					return getLeft().contains(elem);
+				else
+					return false;
+			}
+			//Unreachable statement.
+			return false;
+
+
+		}
+
+
+		// Recursive method for writing the tree to  a dot file
+		public void writeDot(FileWriter output) throws Exception
+		{
+			output.write(this.data + "[label=\"<L> |<D> " + this.data + "|<R> \"]\n");
+
+			if(this.left != null)
+			{
+				// write the left subtree
+				this.left.writeDot(output);
+
+				// then make a link between n and the left subtree
+				output.write(this.data + ":L -> " + this.left.data + ":D\n" );
+			}
+			if(this.right != null)
+			{
+				// write the left subtree
+				this.right.writeDot(output);
+
+				// then make a link between n and the right subtree
+				output.write(this.data + ":R -> " + this.right.data + ":D\n" );
+			}		
+		}
+
+	}
+
 }
+
