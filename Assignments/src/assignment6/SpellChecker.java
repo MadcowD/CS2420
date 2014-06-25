483d9ade999cae190725d60caf386f23405c1810
MadcowD <carlostnb@gmail.com>
2014-06-25 13:43:52 -0600

    Finished SpellChecker.java. tested and working.

--- Assignments/src/assignment6/SpellChecker.java
+++ Assignments/src/assignment6/SpellChecker.java
@@ -22,29 +22,37 @@ public class SpellChecker
 		File dictionary = null;
 		File document = null;
 		String option = "";
+
+
+		if(args.length < 2 || args.length > 3)
+		{
+			System.out.println("Incorrect number of arguments!");
+			return;
+		}
 		
-		//TODO: Check parameter size according to the handout
+		dictionary = new File(args[0]);
+		if(!dictionary.isFile()){
+			System.out.println("Unable to use the dictionary file!");
+			return;
+		}
+
 		
-		//TODO: Instantiate the dictionary File object using args[0]		
-		
-		//TODO: Check to see if this dictionary file is a normal file
-		//Use the File class isFile() method
-		
-		//TODO: Instantiate the document File object using args[1]		
-		
-		//TODO: Check to see if this document file is a normal file
-		//Use the File class isFile() method		
+		document = new File(args[1]);
+		if(!document.isFile()){
+			System.out.println("Unable to use the document file!");
+			return;
+		}
 		
 		// If a third parameter was passed for the options, check its validity 
 		if (args.length == 3) 
-   			if(args[2].equalsIgnoreCase("-p") || args[2].equalsIgnoreCase("-f"))
-   				option = args[2];
-   			else 
-   			{
-   				System.out.println("Invalid printing or filing option argument!");
-   				return;
-   			}
-		
+			if(args[2].equalsIgnoreCase("-p") || args[2].equalsIgnoreCase("-f"))
+				option = args[2];
+			else 
+			{
+				System.out.println("Invalid printing or filing option argument!");
+				return;
+			}
+
 		// Passing the dictionary file, document file, and the option
 		run_spell_check(dictionary, document, option);		
 	}
@@ -53,37 +61,39 @@ public class SpellChecker
 	{
 		// Creating a new SpellCheckerUtil object with the dictionary file
 		SpellCheckUtil mySC = new SpellCheckUtil(dic);
-		
+
 		// Creating a list of misspelled words after checking spellcheking the document
 		List<String> misspelledWords = mySC.spellCheck(doc);
-	   
+
 		if (misspelledWords.size() == 0) 
 			System.out.println("\nThere are no misspelled words in file " + doc + ".\n");
 		else 
 		{
 			System.out.println("\nThere are " + misspelledWords.size() + " misspelled words in file " + doc + ".");
-	      
+
 			if(option.equals("-p"))
 			{
-				//TODO: Print every misspelled word on a new line
+				for(String word : misspelledWords)
+					System.out.println(word);
 			}
 			else if(option.equals("-f"))				
 				try
-				{
+			{
 					FileWriter writer = new FileWriter("misspelled.txt");
+
+					for(String word : misspelledWords)
+						writer.write(word + "\n");
 					
-					//TODO: Put every misspelled word on a new line in the misspelled.txt file 
-					
-					//TODO: Make sure to close the "writer" file you just populated.
-					
+					writer.close();
+
 					System.out.println("Please see misspelled.txt for a list of the words.");	
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("Unable to create a file for the misspelled words!");
-					return;
-				}
-		
+			} 
+			catch (IOException e) 
+			{
+				System.out.println("Unable to create a file for the misspelled words!");
+				return;
+			}
+
 			System.out.println("\nHave a nice day!\n");		
 		}		
 	}
