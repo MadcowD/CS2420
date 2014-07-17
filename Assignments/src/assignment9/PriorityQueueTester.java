45ec2cf3c8fdccdaf0ebd1446d1382ce4befe3b5
Maks Cegielski-Johnson <maksus01@hotmail.com>
2014-07-17 13:11:03 -0600

    i think testing is finished

--- Assignments/src/assignment9/PriorityQueueTester.java
+++ Assignments/src/assignment9/PriorityQueueTester.java
@@ -3,12 +3,14 @@ package assignment9;
 import junit.framework.TestCase;
 
 /**
- * @author Maks
+ * @author Maks Cegielski-Johnson
+ * @author William Guss
  *
  */
 public class PriorityQueueTester extends TestCase {
 
 	PriorityQueueHEAP<Integer> heap = new PriorityQueueHEAP<Integer>();
+	PriorityQueueBST<Integer> bst = new PriorityQueueBST<Integer>();
 	/**
 	 * Test method for {@link assignment9.PriorityQueueHEAP#size()}.
 	 */
@@ -25,6 +27,20 @@ public class PriorityQueueTester extends TestCase {
 		heap.deleteMin();
 		assertEquals(heap.size(), 0);
 	}
+	
+	public void testSizeBST(){
+		assertEquals(bst.size(), 0);
+		bst.add(3);
+		assertEquals(bst.size(), 1);
+		bst.add(4);
+		assertEquals(bst.size(), 2);
+		bst.add(3);
+		assertEquals(bst.size(), 2);
+		bst.deleteMin();
+		bst.deleteMin();
+		assertEquals(bst.size(), 0):
+		
+	}
 
 	/**
 	 * Test method for {@link assignment9.PriorityQueueHEAP#clear()}.
@@ -37,17 +53,34 @@ public class PriorityQueueTester extends TestCase {
 		heap.clear();
 		assertEquals(heap.size(), 0);
 	}
+	
+	public void testClearBST(){
+		assertEquals(bst.size(), 0);
+		for(int i = 0; i < 5; i++)
+			bst.add(i);
+		assertEquals(bst.size(), 5);
+		bst.clear();
+		assertEquals(bst.size(), 0);
+	}
 
 	/**
 	 * Test method for {@link assignment9.PriorityQueueHEAP#isEmpty()}.
 	 */
-	public void testIsEmpty() {
+	public void testIsEmptyHeap() {
 		assertTrue(heap.isEmpty());
 		heap.add(1);
 		assertTrue(!heap.isEmpty());
 		heap.clear();
 		assertTrue(heap.isEmpty());
 		
+	}
+	
+	public void testIsEmptyBST(){
+		assertTrue(bst.isEmpty());
+		bst.add(1);
+		assertTrue(!bst.isEmpty());
+		bst.clear();
+		assertTrue(bst.isEmpty());
 	}
 
 	/**
@@ -71,6 +104,30 @@ public class PriorityQueueTester extends TestCase {
 		assertEquals(heap.findMin(), new Integer(14));
 		heap.deleteMin();
 		assertEquals(heap.findMin(), new Integer(24));
+		heap.deleteMin();
+		assertEquals(heap.findMin(), new Integer(24));
+		heap.deleteMin();
+		assertEquals(heap.findMin(), new Integer(25));
+		heap.deleteMin();
+		assertEquals(heap.size(), 0);
+	}
+	
+	public void testFindMinBST(){
+		PriorityQueueBST<Integer> heap = bst; //changed name decleration for this test; TODO fix in eclipse
+		heap.add(10);
+		heap.add(13);
+		heap.add(2);
+		heap.add(24);
+		heap.add(25);
+		heap.add(14);
+		
+		assertEquals(heap.findMin(), new Integer(2));
+		heap.deleteMin();
+		assertEquals(heap.findMin(), new Integer(10));
+		heap.deleteMin();
+		assertEquals(heap.findMin(), new Integer(13));
+		heap.deleteMin();
+		assertEquals(heap.findMin(), new Integer(14));
 		heap.deleteMin();
 		assertEquals(heap.findMin(), new Integer(24));
 		heap.deleteMin();
@@ -102,16 +159,46 @@ public class PriorityQueueTester extends TestCase {
 		assertEquals(heap.size(), 0);
 	}
 
+
+	public void testDeleteMinBST(){
+		PriorityQueueBST<Integer> heap = bst; //TODO
+		heap.add(10);
+		heap.add(13);
+		heap.add(2);
+		heap.add(24);
+		heap.add(25);
+		heap.add(24);
+		heap.add(14);
+		
+		
+		assertEquals(heap.deleteMin(), new Integer(2));
+		assertEquals(heap.deleteMin(), new Integer(10));
+		assertEquals(heap.deleteMin(), new Integer(13));
+		assertEquals(heap.deleteMin(), new Integer(14));
+		assertEquals(heap.deleteMin(), new Integer(24));
+		assertEquals(heap.deleteMin(), new Integer(24));
+		assertEquals(heap.deleteMin(), new Integer(25));
+		assertEquals(heap.size(), 0);
+	}
 	/**
 	 * Test method for {@link assignment9.PriorityQueueHEAP#add(java.lang.Object)}.
 	 */
-	public void testAdd() {
+	public void testAddHeap() {
 		assertEquals(heap.size(), 0);
 		heap.add(13);
 		assertEquals(heap.size(), 1);
 		assertEquals(heap.findMin(), new Integer(13));
 		
 	}
+	
+	public void testAddBST(){
+		assertEquals(bst.size(), 0);
+		bst.add(13);
+		assertEquals(heap.size(), 1);
+		assertEquals(heap.findMin(), new Integer(13));
+	}
+	
+	
 
 	/**
 	 * Test method for {@link assignment9.PriorityQueueHEAP#toArray()}.
@@ -127,8 +214,22 @@ public class PriorityQueueTester extends TestCase {
 		
 		for(int i = 0; i < 5; i++)
 			assertEquals(compare[i], heapArr[i]);
+	}
+	
+		public void testToArrayBST(){
+		Object[] compare = {14, 1, 5, 3, 12};//I think this is right. TODO check eclipse
+		bst.add(14);
+		bst.add(1);
+		bst.add(5);
+		bst.add(12);
+		bst.add(3);
 		
+		Object[] bstArr = bst.toArray();
 		
+		for(int i = 0; i < 5; i++)
+			assertEquals(compare[i], bstArr[i]);
+	
+			
 	}
 
 }
